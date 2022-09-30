4e3be2cbb55189376af3e8881180033b1b00088d
N-Crypt3d <89503458+N-Crypt3d@users.noreply.github.com>
2022-09-30 11:45:48 +0600

    Add sound Effects

--- app/src/main/java/com/example/soundaudiotutorial/MainActivity.java
+++ app/src/main/java/com/example/soundaudiotutorial/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.soundaudiotutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureSoundActivityButton();
+    }
+
+    private void configureSoundActivityButton(){
+        Button next = (Button) findViewById(R.id.trySoundButton);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SoundEffectActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/soundaudiotutorial/SoundEffectActivity.java
@@ -0,0 +1,92 @@
+package com.example.soundaudiotutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SoundEffectActivity extends AppCompatActivity {
+
+    // soundpool variables
+    private SoundPool soundPool;
+    private int sound1, sound2, sound3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_effect);
+
+        configureSoundEffects();
+        configureBackButton();
+    }
+
+    private void configureSoundEffects(){
+        // potential Error
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes. USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(3)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        } else {
+            soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+        }
+        sound1 = soundPool.load(this, R.raw.sound1, 1);
+        sound2 = soundPool.load(this, R.raw.sound2, 1);
+        sound3 = soundPool.load(this, R.raw.sound3, 1);
+        playSound();
+    }
+
+    public void playSound(){
+        Button effect1 = (Button) findViewById(R.id.effect1);
+        effect1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound1, 1, 1, 1, 0, 1);
+            }
+        });
+
+        Button effect2 = (Button) findViewById(R.id.effect2);
+        effect2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound2, 1, 1, 1, 0, 1);
+            }
+        });
+
+        Button effect3 = (Button) findViewById(R.id.effect3);
+        effect3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound3, 1, 1, 1, 0, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        soundPool.release();
+        soundPool = null;
+    }
+
+    private void configureBackButton(){
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
